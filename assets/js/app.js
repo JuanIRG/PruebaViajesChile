f85da169c730e230ed8ba77b972bea9cdcb528bd
Juan Reyes <juanignacioreyesg@gmail.com>
2021-12-03 22:11:48 -0300

    Se corrige navegacion en el nav, el modal de la seccion destacado cambia diseño pequeños detalles del typeform (padding)

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,17 @@ $(document).ready(function () {
         $("#formulario").toggle("slow");
     });
 });
+
+$(document).ready(function () {
+
+    $("a").click(function () {
+        var gato = this.hash
+
+        $("html,body").animate({
+                scrollTop: $(gato).offset().top - 70
+            }, 800
+
+        )
+
+    });
+});
